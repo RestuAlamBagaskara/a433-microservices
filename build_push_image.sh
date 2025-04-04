09b00bd53fd79457111dee2df1a7602249e571ab
bagaskara <alambagaskara18@gmail.com>
2025-04-04 17:14:03 +0700

    Submission Proyek 1

--- build_push_image.sh
+++ build_push_image.sh
@@ -7,12 +7,12 @@ set -e
 GITHUB_USERNAME="restualambagaskara"  # Ganti dengan username GitHub Anda
 REPO_NAME="item-app"                    # Nama repository GitHub
 IMAGE_TAG="v1"
+LOCAL_IMAGE_NAME="$REPO_NAME:$IMAGE_TAG" 
 IMAGE_NAME="ghcr.io/$GITHUB_USERNAME/$REPO_NAME:$IMAGE_TAG"
-REMOTE_IMAGE_NAME="$GITHUB_USERNAME/$REPO_NAME:$IMAGE_TAG"
 
 # Build Docker image
-echo "Membuat Docker image: $IMAGE_NAME..."
-docker build -t $IMAGE_NAME .
+echo "Membuat Docker image: $LOCAL_IMAGE_NAME..."
+docker build -t $LOCAL_IMAGE_NAME .
 
 # Menampilkan daftar image di lokal
 echo "Daftar Docker image lokal:"
@@ -20,7 +20,7 @@ docker images
 
 # Mengubah nama Image
 echo "Menandai ulang image ke format Docker Hub..."
-docker tag $IMAGE_NAME $REMOTE_IMAGE_NAME
+docker tag $LOCAL_IMAGE_NAME $IMAGE_NAME
 
 # Login ke GitHub Container Registry (ghcr.io)
 echo "Login ke GitHub Packages..."

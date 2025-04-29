4f32fe4b54b6572e787d4032a356959696093394
Alam <alambagaskara18@gmail.com>
2025-04-29 23:10:07 +0700

    Submission 3

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Gunakan image Node.js resmi
+FROM node:20-alpine
+
+# Set working directory
+WORKDIR /app
+
+# Copy file package.json dan package-lock.json (kalau ada)
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy seluruh project
+COPY . .
+
+# Expose port (sesuaikan dengan PORT yang ada di .env)
+EXPOSE 3000
+
+# Jalankan aplikasi
+CMD ["node", "index.js"]
---
+++ build_push_image_order-service.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Hentikan script jika terjadi error
+set -e
+
+# Variabel konfigurasi
+GITHUB_USERNAME="restualambagaskara"  # Ganti dengan username GitHub Anda
+REPO_NAME="dicoding-order-service"                 # Nama repository GitHub
+IMAGE_TAG="latest"
+LOCAL_IMAGE_NAME="$REPO_NAME:$IMAGE_TAG" 
+IMAGE_NAME="ghcr.io/$GITHUB_USERNAME/$REPO_NAME:$IMAGE_TAG"
+
+# Build Docker image
+echo "Membuat Docker image: $LOCAL_IMAGE_NAME..."
+docker build -t $LOCAL_IMAGE_NAME .
+
+# Menampilkan daftar image di lokal
+echo "Daftar Docker image lokal:"
+docker images
+
+# Mengubah nama Image
+echo "Menandai ulang image ke format Docker Hub..."
+docker tag $LOCAL_IMAGE_NAME $IMAGE_NAME
+
+# Login ke GitHub Container Registry (ghcr.io)
+echo "Login ke GitHub Packages..."
+echo $PAT | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
+
+# Push Docker image ke GitHub Packages
+echo "Mengunggah image ke GitHub Packages..."
+docker push $IMAGE_NAME
+
+echo "Image berhasil diunggah ke GitHub Packages: $IMAGE_NAME"
